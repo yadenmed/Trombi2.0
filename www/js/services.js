6c74d804877f2bfc75b3d565de984ee6b97634e7
unknown <myaden@GA10002.groupe-atlantic.com>
2015-04-02 15:17:26 +0200

    i hope it's the last fix

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('directory.services', ['ngResource'])
 
     .factory('Employees', function ($resource) {
-        return $resource('http://localhost:8000/employees/:employeeId/:data');
+        return $resource('http://atlantictrombi.herokuapp.com/employees/:employeeId/:data');
     });
